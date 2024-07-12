9c1d0bb1c5b275287aebad44aebe3eceb34f1331
syed zohaib <zohaibsyed914@gmail.com>
2024-07-12 17:07:11 +0500

    local working

--- home.js
+++ home.js
@@ -1,72 +1,3 @@
-// const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'))
-
-// if (!loggedInUser) {
-//     Swal.fire("No user is logged in");
-//     setTimeout(() => {
-//         window.location.href = 'login.html'
-//     }, 1000)
-
-// } else {
-//     document.getElementById('username').textContent = loggedInUser.username
-// }
-
-// document.getElementById('logoutButton').addEventListener('click', function () {
-//     localStorage.removeItem('loggedInUser')
-//     Swal.fire('Logged out successfully')
-
-//     setTimeout(() => {
-//         window.location.href = 'login.html'
-//     }, 1000)
-
-
-
-// })
-// // const cardBox = document.getElementsByClassName('cardBox')
-// const cardInput = document.getElementById('cardInput')
-// const cardSubmit = document.getElementById('cardSubmit')
-
-// // cardInput.textContent = ""
-
-
-
-// cardSubmit.addEventListener('click', function () {
-
-//     console.log('button chala')
-//     const cardInputValue = cardInput.value
-
-//     localStorage.setItem('inputkivalue', cardInputValue)
-
-//     const cardBoxy = document.createElement('div')
-//     cardBoxy.classList.add('cardBox')
-//     // alert(inputValue)
-//     const cradDiv = `<div class="card">
-//             <div class="cardPara">
-//                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, deserunt?</p>
-//             </div>
-//             <div class="auther">Creater by.. <span id="createUser"></span></div>
-//         </div>`
-//     cardBoxy.innerHTML = cradDiv
-//     // const cardDiv = document.createElement('div')
-//     // cardDiv.classList.add('card')
-//     // // cardDiv.style.border = '1px solid black'
-//     // // const cardPara = document.createElement(div)
-//     // // localStorage.getItem('inputkivalue', cardInputValue)
-//     document.getElementById('createUser').textContent = loggedInUser.username
-
-//     // const cardData = ` <div class="cardPara">
-//     //             <p id="inputUser"></p>
-//     //         </div>
-//     //         <div class="auther">Creater by.. <span id="createUser"></span></div>`
-
-//     // cardDiv.innerHTML = cardData
-
-
-//     document.body.appendChild(cardBoxy)
-
-
-// })
-
-
 const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
 if (!loggedInUser) {
@@ -76,6 +7,10 @@ if (!loggedInUser) {
     }, 1000)
 
 } else {
+    if (!loggedInUser.posts) {
+        loggedInUser.posts = []
+    }
+
     document.getElementById('username').textContent = loggedInUser.username
 }
 
@@ -89,44 +24,130 @@ document.getElementById('logoutButton').addEventListener('click', function () {
     }, 1000);
 });
 
-const cardInput = document.getElementById('cardInput');
-const cardSubmit = document.getElementById('cardSubmit');
-const cardBox = document.querySelector('.cardBox')
+// ---------------------------------------------------------------------------------------------------------------
 
+function displayPosts() {
+    const postsSection = document.getElementById('posts')
+    postsSection.innerHTML = ""
 
-cardSubmit.addEventListener('click', function () {
-    console.log('button chala');
-    const cardInputValue = cardInput.value;
+    if (loggedInUser && loggedInUser.posts) {
+        loggedInUser.posts.forEach((post, index) => {
+            const postElement = document.createElement('div')
+            postElement.classList.add('card')
+            postElement.innerHTML = `
+            
+            <div class="auther">Creater by.. <span>${post.author}</span></div>
 
+            <div class="cardPara">
+                <p>${post.content}</p>
+            </div>
+            <div class="btnGroup">
+                <button type="button" class="edit" onclick="editPost(${index})"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button type="button" class="delete" onclick="deletePost(${index})"><i class="fa-solid fa-trash"></i></button>
+            </div>
+            <div class="dateStamp">${post.createdAt}</div>
+            `
+            postsSection.appendChild(postElement)
 
+        })
+    }
+}
 
-    if (!cardInputValue) {
-        Swal.fire("PLz fill out this field")
-    } else {
-
-        const cards = JSON.parse(localStorage.getItem('cards')) || []
-        cards.push({ cardInputValue })
-        localStorage.setItem('cards', JSON.stringify(cards))
-
-
-
-        // localStorage.setItem('inputkivalue', cardInputValue);
-        const showCard = JSON.parse(localStorage.getItem('cards'))
-        const cardBoxy = document.createElement('div');
-        cardBoxy.classList.add('card');
-        const cardDiv = `
-                <div class="cardPara">
-                    <p>${showCard}</p>
-                </div>
-                <div class="auther">Created by.. <span class="createUser">${loggedInUser.username}</span></div>`;
-        cardBoxy.innerHTML = cardDiv;
-
-        cardBox.appendChild(cardBoxy);
-
+document.getElementById("cardSubmit")?.addEventListener('click', function () {
+    const content = document.getElementById("cardInput").value.trim()
+    if (!content) {
+        Swal.fire("plz enter some content")
+        return
     }
 
+    setTimeout(() => {
+        const post = {
+            author: loggedInUser.username,
+            content: content,
+            createdAt: new Date().toLocaleString()
 
-});
+        }
+
+        loggedInUser.posts.unshift(post)
+        // console.log(loggedInUser.post.unshift(post))
+        localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+
+        document.getElementById('cardInput').value = ""
+        displayPosts()
+
+
+
+    }, 1000)
+})
+
+function editPost(index) {
+    const newContent = prompt(
+        "Edit Yout Post",
+        loggedInUser.posts[index].content
+    )
+    if (newContent !== null) {
+        loggedInUser.posts[index].content = newContent
+        localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+        displayPosts()
+    }
+}
+
+function deletePost(index) {
+    if (Swal.fire("Are you sure you want to delete your post")) {
+        loggedInUser.posts.splice(index, 1)
+        localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+        displayPosts()
+    }
+}
+
+if (document.getElementById("homePage")) {
+    // document.getElementById("homePage").classList.remove("hidden");
+    document.getElementById("usernameDisplay").textContent = currentUser.username;
+    displayPosts();
+}
+
+
+
+// const cardInput = document.getElementById('cardInput');
+// const cardSubmit = document.getElementById('cardSubmit');
+// const cardBox = document.querySelector('.cardBox')
+
+
+// cardSubmit.addEventListener('click', function () {
+//     console.log('button chala');
+//     const cardInputValue = cardInput.value;
+
+
+
+//     if (!cardInputValue) {
+//         Swal.fire("PLz fill out this field")
+//     } else {
+
+//         const cards = JSON.parse(localStorage.getItem('cards')) || []
+//         cards.push({ cardInputValue })
+//         localStorage.setItem('cards', JSON.stringify(cards))
+
+
+
+
+
+//         JSON.parse(localStorage.getItem('cards'))
+//         // console.log(showCard[0])
+//         const cardBoxy = document.createElement('div');
+//         cardBoxy.classList.add('card');
+//         const cardDiv = `
+//                 <div class="cardPara">
+//                     <p>${cards}</p>
+//                 </div>
+//                 <div class="auther">Created by.. <span class="createUser">${loggedInUser.username}</span></div>`;
+//         cardBoxy.innerHTML = cardDiv;
+
+//         cardBox.appendChild(cardBoxy);
+
+//     }
+
+
+// });
 
 
 
